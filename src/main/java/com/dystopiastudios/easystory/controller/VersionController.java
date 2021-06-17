78fd11f9bd6e54257704dafaf8aec5ebb40598a1
gonchox <57024307+gonchox@users.noreply.github.com>
2021-06-16 23:59:20 -0500

    Update VersionController.java

--- src/main/java/com/dystopiastudios/easystory/controller/VersionController.java
+++ src/main/java/com/dystopiastudios/easystory/controller/VersionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://easystory-open.web.app")
+@CrossOrigin(origins = "https://easystory-85267.web.app")
 public class VersionController {
     @GetMapping("/version")
     public String getVersion() {
