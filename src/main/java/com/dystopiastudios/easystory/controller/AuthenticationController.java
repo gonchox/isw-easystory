022a1fff58fb4411556b409ea304f38db058f467
gonchox <57024307+gonchox@users.noreply.github.com>
2021-06-16 23:57:50 -0500

    Update AuthenticationController.java

--- src/main/java/com/dystopiastudios/easystory/controller/AuthenticationController.java
+++ src/main/java/com/dystopiastudios/easystory/controller/AuthenticationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Objects;
 
 @RestController
-@CrossOrigin(origins = "https://easystory-open.web.app")
+@CrossOrigin(origins = "https://easystory-85267.web.app")
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     @Autowired
