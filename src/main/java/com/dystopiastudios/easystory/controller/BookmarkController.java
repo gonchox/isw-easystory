f8e0a874ea11e5777e13bcb4cb16f273de9fcffc
gonchox <57024307+gonchox@users.noreply.github.com>
2021-06-16 23:58:04 -0500

    Update BookmarkController.java

--- src/main/java/com/dystopiastudios/easystory/controller/BookmarkController.java
+++ src/main/java/com/dystopiastudios/easystory/controller/BookmarkController.java
@@ -26,7 +26,7 @@ import java.util.stream.Collectors;
 @Tag(name = "bookmarks", description = "Bookmarks desc")
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://easystory-open.web.app")
+@CrossOrigin(origins = "https://easystory-85267.web.app")
 public class BookmarkController {
 
     @Autowired
