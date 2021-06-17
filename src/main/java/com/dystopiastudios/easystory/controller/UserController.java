3855a9002749c5ceef83b55f335f6c633d742e33
gonchox <57024307+gonchox@users.noreply.github.com>
2021-06-16 23:59:10 -0500

    Update UserController.java

--- src/main/java/com/dystopiastudios/easystory/controller/UserController.java
+++ src/main/java/com/dystopiastudios/easystory/controller/UserController.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
 @Tag(name = "users", description = "Users API")
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://easystory-open.web.app")
+@CrossOrigin(origins = "https://easystory-85267.web.app")
 public class UserController {
     @Autowired
     private ModelMapper mapper;
